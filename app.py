a5e3235405f9818b0f475ec2a8993b21b2b78ab6
“ogeeeen” <ogeeeen24@gmail.com>
2023-05-01 20:58:00 +0900

    Update application files

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import base64
 import cv2
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
-import os
+import os  # 追加
 
 app = Flask(__name__)
 CORS(app)
@@ -19,36 +19,18 @@ def index():
 
 @app.route("/learn", methods=["POST"])
 def learn():
-    try:
-        data = request.json["data"]
-        class_id = request.json["classId"]  # キー名を修正
-        training_data.append(data)
-        labels.append(class_id)
-        knn.fit(training_data, labels)
-        return jsonify({"status": "ok"})
-
-    except Exception as e:
-        print(str(e))
-        response = {
-            "success": False,
-            "message": "An error occurred during processing",
-        }
-        return jsonify(response), 500
+    data = request.json["data"]
+    class_id = request.json["class_id"]
+    training_data.append(data)
+    labels.append(class_id)
+    knn.fit(training_data, labels)
+    return jsonify({"status": "ok"})
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    try:
-        data = request.json["data"]
-        probabilities = knn.predict_proba([data])[0]
-        return jsonify({"probabilities": probabilities.tolist()})
-
-    except Exception as e:
-        print(str(e))
-        response = {
-            "success": False,
-            "message": "An error occurred during processing",
-        }
-        return jsonify(response), 500
+    data = request.json["data"]
+    probabilities = knn.predict_proba([data])[0]
+    return jsonify({"probabilities": probabilities.tolist()})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
--- static/js/main.js
+++ static/js/main.js
@@ -1,48 +1,55 @@
 const video = document.getElementById("video");
-const canvas = document.createElement("canvas");
-const ctx = canvas.getContext("2d");
+const buttons = document.querySelectorAll(".class-btn");
+const predictButton = document.getElementById("predict");
+const output = document.getElementById("output");
 
-canvas.width = 480;
-canvas.height = 360;
-
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then(function (stream) {
-      video.srcObject = stream;
-      video.play();
-    });
+async function startCamera() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+  } catch (err) {
+    console.error("Error: " + err);
+  }
 }
 
-const buttons = document.querySelectorAll(".class-btn");
+startCamera();
 
 buttons.forEach((button) => {
   button.addEventListener("click", async () => {
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const response = await postData("/learn", {
-      classId: button.id,
-      data: Array.from(imageData.data),
+    const classId = parseInt(button.dataset.classId);
+    const data = await captureData();
+    postData("/learn", { data, class_id: classId }).then((res) => {
+      console.log(res);
     });
-    if (response.success) {
-      document.getElementById(`count${button.id.slice(-1)}`).textContent =
-        `学習枚数: ${response.count}`;
-    } else {
-      console.error("Error during learning");
-    }
   });
 });
+
+predictButton.addEventListener("click", async () => {
+  const data = await captureData();
+  postData("/predict", { data }).then((res) => {
+    const probabilities = res.probabilities;
+    const maxIndex = probabilities.reduce((iMax, x, i, arr) => (x > arr[iMax] ? i : iMax), 0);
+    output.innerText = `Class ${maxIndex + 1}: ${(probabilities[maxIndex] * 100).toFixed(2)}%`;
+  });
+});
+
+async function captureData() {
+  const imageCapture = new ImageCapture(video.srcObject.getVideoTracks()[0]);
+  const bitmap = await imageCapture.grabFrame();
+  const imageData = new ImageData(bitmap.width, bitmap.height);
+  imageData.data.set(bitmap.data);
+  const data = Array.from(imageData.data);
+  return data;
+}
 
 async function postData(url = "", data = {}) {
   const response = await fetch(url, {
     method: "POST",
-    mode: "cors",
-    cache: "no-cache",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ data: data }), // 修正後のコード
+    body: JSON.stringify(data),
   });
 
-  return response.json();
+  return await response.json();
 }
