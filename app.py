ccf60c9cfc302694a8350b061cb17df9e3bdf243
“ogeeeen” <ogeeeen24@gmail.com>
2023-05-01 21:56:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,27 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-import base64
-import cv2
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
-import os  # 追加
 
 app = Flask(__name__)
 CORS(app)
 
 knn = KNeighborsClassifier(n_neighbors=3)
-training_data = []
-labels = []
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route('/classify', methods=['POST'])
+def classify():
+    data = request.get_json()
+    input_data = np.array(data['inputData']).reshape(1, -1)
+    prediction = knn.predict(input_data)
+    return jsonify({'prediction': int(prediction[0])})
 
-@app.route("/learn", methods=["POST"])
+@app.route('/learn', methods=['POST'])
 def learn():
-    data = request.json["data"]
-    class_id = request.json["class_id"]
-    training_data.append(data)
-    labels.append(class_id)
+    data = request.get_json()
+    training_data = np.array(data['trainingData']).reshape(-1, 1)
+    labels = np.array(data['labels'])
     knn.fit(training_data, labels)
-    return jsonify({"status": "ok"})
+    return jsonify({'success': True})
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.json["data"]
-    probabilities = knn.predict_proba([data])[0]
-    return jsonify({"probabilities": probabilities.tolist()})
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+if __name__ == '__main__':
+    app.run()
