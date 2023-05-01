7fb350644dfb3d3492fc2853c72979c54eac5852
“ogeeeen” <ogeeeen24@gmail.com>
2023-05-01 13:21:40 +0900

    Update index.html and main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,10 @@
 window.onload = function () {
-  const learnButtons = document.querySelectorAll(".learn-button");
-  const predictButton = document.querySelector("#predict-button");
+  const classButtons = document.querySelectorAll(".class-btn");
   const resultElement = document.querySelector("#result");
 
-  learnButtons.forEach((button) => {
+  classButtons.forEach((button) => {
     button.addEventListener("click", async () => {
-      const classId = parseInt(button.dataset.classId, 10);
+      const classId = parseInt(button.id.replace('class', ''), 10);
       const canvas = document.querySelector("#canvas");
       const context = canvas.getContext("2d");
       const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
@@ -23,26 +22,6 @@ window.onload = function () {
         console.error("Error during learning");
       }
     });
-  });
-
-  predictButton.addEventListener("click", async () => {
-    const canvas = document.querySelector("#canvas");
-    const context = canvas.getContext("2d");
-    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    const sample = preprocess(imageData);
-
-    const response = await fetch("/predict", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ data: sample }),
-    });
-
-    if (response.ok) {
-      const { probabilities } = await response.json();
-      displayProbabilities(probabilities);
-    } else {
-      console.error("Error during prediction");
-    }
   });
 
   function preprocess(imageData) {
