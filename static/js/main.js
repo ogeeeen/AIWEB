4eaa8506a2d4a29278d3d99a2cbc62f0cfc215a0
“ogeeeen” <ogeeeen24@gmail.com>
2023-05-01 16:17:35 +0900

    Fix endpoint in main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,48 @@
-window.onload = function () {
-  const classButtons = document.querySelectorAll(".class-btn");
-  const resultElement = document.querySelector("#result");
+const video = document.getElementById("video");
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
 
-  classButtons.forEach((button) => {
-    button.addEventListener("click", async () => {
-      const classId = parseInt(button.id.replace('class', ''), 10);
-      const canvas = document.querySelector("#canvas");
-      const context = canvas.getContext("2d");
-      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-      const sample = preprocess(imageData);
+canvas.width = 480;
+canvas.height = 360;
 
-      const response = await fetch("/learn", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ data: sample, class_id: classId }),
-      });
-
-      if (response.ok) {
-        console.log(`Learned class ${classId}`);
-      } else {
-        console.error("Error during learning");
-      }
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+      video.play();
     });
+}
+
+const buttons = document.querySelectorAll(".class-btn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", async () => {
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const response = await postData("/learn", {
+      classId: button.id,
+      data: Array.from(imageData.data),
+    });
+    if (response.success) {
+      document.getElementById(`count${button.id.slice(-1)}`).textContent =
+        `学習枚数: ${response.count}`;
+    } else {
+      console.error("Error during learning");
+    }
+  });
+});
+
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ data: data }), // 修正後のコード
   });
 
-  function preprocess(imageData) {
-    // ...（前処理コードは変更されていません）
-  }
-
-  function displayProbabilities(probabilities) {
-    // ...（結果表示コードは変更されていません）
-  }
-};
+  return response.json();
+}
