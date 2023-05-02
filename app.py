30dba363b7070f29ac1b19ecbe725b0661b2896b
“ogeeeen” <ogeeeen24@gmail.com>
2023-05-02 18:20:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from sklearn.neighbors import KNeighborsClassifier
@@ -6,18 +7,22 @@ import numpy as np
 app = Flask(__name__)
 CORS(app)
 
+logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+
 knn = KNeighborsClassifier(n_neighbors=3)
 
 @app.route('/learn', methods=['POST'])
 def learn():
+    app.logger.info('Learn route has been accessed')
     data = request.json
     training_data = [list(d.values()) for d in data['training_data']]
     labels = data['labels']
     knn.fit(training_data, labels)
     return jsonify({"status": "success"})
-#️　変更
+
 @app.route('/predict', methods=['POST'])
 def predict():
+    app.logger.info('Predict route has been accessed')
     data = request.json
     input_data = np.array(data['input']).reshape(1, -1)
     result = knn.predict(input_data)
